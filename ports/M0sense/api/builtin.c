aedd301c94d93f8a3b5874415ecca5a6688ed898
YahirGracia0927 <yegracia@udistrital.edu.co>
2024-10-07 16:04:16 -0500

    Update M0sense

--- ports/M0sense/api/builtin.c
+++ ports/M0sense/api/builtin.c
@@ -5,14 +5,6 @@
 // License: MIT
 //
 // Description: Builtin definitions (Explorer16-PIC24 port)
-#define led3    a0  // Onboard LEDs            
-#define led4    a1        
-#define led5    a2        
-#define led6    a3       
-#define led7    a4       
-#define led8    a5        
-#define led9    a6        
-#define led10   a7    
 
 #define sw3     d6_i  // Onboard switchs  
 #define sw4     d13_i  
--- ports/M0sense/api/builtin.c.v
+++ ports/M0sense/api/builtin.c.v
@@ -4,9 +4,6 @@
 // License: MIT
 //
 // Description: Builtin definitions (M0sense port)
-module main
-
-
 
 #include <bflb_platform.h>
 #include <hal_gpio.h>
@@ -21,5 +18,53 @@ module main
 #include "io_def.h"
 #include "builtin.c"
 
-fn C.init()
+module main
 
+enum Pin_names {
+
+gpio0 = 0    
+    gpio1
+    gpio2
+    gpio3
+    gpio4
+    gpio5
+    gpio6
+    gpio7
+    gpio8
+    gpio9
+    gpio10
+    gpio11
+    gpio12
+    gpio13
+    gpio14
+    gpio15
+    gpio16
+    gpio17
+    gpio18
+    gpio19
+    gpio20
+    gpio21
+    gpio22
+    gpio23
+    gpio24
+    gpio25
+    gpio26
+    gpio27
+    gpio28
+  
+}
+
+enum Builtin_names {
+	LED_B_PIN = 23
+}
+
+enum Pin_modes {
+	input = 4
+	output = 1
+	in_pullup
+}
+
+enum ADC_pin_names {
+	gpio = 27
+	
+}
